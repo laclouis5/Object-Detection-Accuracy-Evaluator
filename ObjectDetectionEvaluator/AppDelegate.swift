494bab4b39dd5d690f462184d09c699aea12e36c
Louis Lac <lac.louis5@gmail.com>
2019-09-07 13:05:50 +0200

    Initial Commit

---
+++ ObjectDetectionEvaluator/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  ObjectDetectionEvaluator
+//
+//  Created by Louis Lac on 07/09/2019.
+//  Copyright © 2019 Louis Lac. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ ObjectDetectionEvaluator/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ObjectDetectionEvaluator
+//
+//  Created by Louis Lac on 07/09/2019.
+//  Copyright © 2019 Louis Lac. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
